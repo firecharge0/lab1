7fddb0520dc9d07a212772c22fa773af5fd430d8
SebastianTorrez <49455042+firecharge0@users.noreply.github.com>
2024-08-28 15:31:02 -0400

    Add files via upload

--- Project1/Time/src/gettime.c
+++ Project1/Time/src/gettime.c
@@ -3,15 +3,21 @@
 #include <stdio.h>
 
 struct Time GetTime() {
-  int hours;
-  int minutes;
-  int seconds;
-  printf("What is the current hour: ");
-  scanf("%d", &hours);
-  printf("What is the current minute: ");
-  scanf("%d", &minutes);
-  printf("What is the current second: ");
-  scanf("%d", &seconds);
+  int hours = 24;
+  int minutes = 60;
+  int seconds = 60;
+  while (hours > 23 || hours < 0) {
+    printf("What is the current hour: ");
+    scanf("%d", &hours);
+  };
+  while (minutes > 59 || minutes < 0) {
+    printf("What is the current minute: ");
+    scanf("%d", &minutes);
+  };
+  while (seconds > 59 || seconds < 0) {
+    printf("What is the current second: ");
+    scanf("%d", &seconds);
+  };
   struct Time b;
   b.hour = hours;
   b.minute = minutes;
